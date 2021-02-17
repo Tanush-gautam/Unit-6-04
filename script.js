8223992c08c144d4077b21a4a188e12f25eac153
Tanushgautam <tanush.gautam@ocsbstudent.ca>
2021-02-17 17:12:28 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let a = 0
+let b = 0
+let h= 0
+
+document.getElementById('calculate').addEventListener('click',formula)
+
+function formula() {
+  a=document.getElementById('baseA').value
+  b=document.getElementById('baseB').value
+  h=document.getElementById('height').value
+
+a = parseInt(a)
+b = parseInt(b)
+h = parseInt(h)
+
+number = (a+b)/2*h
+alert(number)
+}
