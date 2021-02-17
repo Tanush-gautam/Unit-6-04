2a0ae99a19d638ce3872b50838497b774e7c0f82
Tanushgautam <tanush.gautam@ocsbstudent.ca>
2021-02-17 17:49:36 +0000

    wd

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let a = 0
 let b = 0
 let h = 0
-let number = 0 
-document.getElementById('calculate').addEventListener('click',formula)
+let number = 0
+document.getElementById('calculate').addEventListener('click', formula)
 
 function formula () {
   a = document.getElementById('base-a').value
   b = document.getElementById('base-b').value
   h = document.getElementById('height').value
 
-  a = parseInt(a) 
+  a = parseInt(a)
   b = parseInt(b)
   h = parseInt(h)
 
