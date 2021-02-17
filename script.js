8c7a7c731d76009943f6f14d41d8f45affe8264f
Tanushgautam <tanush.gautam@ocsbstudent.ca>
2021-02-17 17:43:05 +0000

    numbers

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let a = 0
 let b = 0
 let h = 0
-
+let number = 0 
 document.getElementById('calculate').addEventListener('click',formula)
 
 function formula () {
@@ -9,10 +9,10 @@ function formula () {
   b = document.getElementById('base-b').value
   h = document.getElementById('height').value
 
-a = parseInt(a)
-b = parseInt(b)
-h = parseInt(h)
+  a = parseInt(a) 
+  b = parseInt(b)
+  h = parseInt(h)
 
-number = (a+b)/2*h
-alert(number)
+  number = (a + b) / 2 * h
+  alert(number)
 }
