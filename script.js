dbb555d621b8ca9de7654844c71cfb5f782b44e1
Tanushgautam <tanush.gautam@ocsbstudent.ca>
2021-02-17 17:20:57 +0000

    spaces

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let a = 0
 let b = 0
-let h= 0
+let h = 0
 
 document.getElementById('calculate').addEventListener('click',formula)
 
-function formula() {
-  a=document.getElementById('baseA').value
-  b=document.getElementById('baseB').value
-  h=document.getElementById('height').value
+function formula () {
+  a =d ocument.getElementById('baseA').value
+  b = document.getElementById('baseB').value
+  h = document.getElementById('height').value
 
 a = parseInt(a)
 b = parseInt(b)
